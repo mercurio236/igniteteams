3a14f19b15f4d1b7c416c8d6d66f884656dbee90
Arley Souto <macx15@hotmail.com>
2023-06-09 17:20:14 -0300

    componente de input feito

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import theme from '@theme/index';
 
 
 import { Groups } from '@screens/Groups';
+import { NewGroup } from '@screens/NewGroup';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,7 +23,7 @@ export default function App() {
       backgroundColor='transparent'
       translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <NewGroup /> : <Loading />}
     </ThemeProvider>
   );
 }
---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import * as S from './styles'
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { Button } from '@components/Button';
+import { Input } from '@components/Input';
+
+export const NewGroup: React.FC = () => {
+    return (
+        <S.Cotainer>
+            <Header showBackButton />
+
+            <S.Content>
+                <S.Icon />
+                <Highlight
+                    title='Nova turma'
+                    subtitle='crie a turma para adicinar pessoas'
+                />
+                <Input />
+                <Button
+                    title='Criar'
+                    style={{ marginTop: 20 }}
+                />
+            </S.Content>
+        </S.Cotainer>
+    )
+}
+
