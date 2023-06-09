53991f6ad14caf71cfe9d42a5a3c345927afce35
Arley Souto <macx15@hotmail.com>
2023-06-09 14:46:36 -0300

    header criado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import * as S from './styles'
+
+
 import logoImg from '@assets/logo.png'
 
+type Props = {
+    showBackButton?: boolean;
+}
 
-
-export const Header: React.FC = () => {
-  return (
-    <S.Container>
-        <S.Logo source={logoImg}/>
-    </S.Container>
-  )
+export const Header: React.FC<Props> = ({ showBackButton = false }) => {
+    return (
+        <S.Container>
+            {
+                showBackButton &&
+                <S.BackButton>
+                    <S.BackIcon />
+                </S.BackButton>
+            }
+            <S.Logo source={logoImg} />
+        </S.Container>
+    )
 }
 
