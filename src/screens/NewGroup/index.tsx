91bf02af69d031556b464074594f94f05054b668
Arley Souto <macx15@hotmail.com>
2023-06-11 18:34:36 -0300

    ajustando exceções

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/native'
+import { Alert } from 'react-native'
 
 import * as S from './styles'
+
+import { AppError } from '@utils/AppError';
+
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Button } from '@components/Button';
@@ -16,12 +20,20 @@ export const NewGroup: React.FC = () => {
 
     async function handleNew() {
         try {
+            if(group.trim().length === 0){
+                return Alert.alert('Novo Grupo', 'Informe o nome da turma')
+            }
 
             await groupCreate(group)
             navigation.navigate('players', { group })
-            
+
         } catch (error) {
-            console.log(error)
+            if (error instanceof AppError) {
+                Alert.alert('Novo Grupo', error.message)
+            }else{
+                Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.')
+                console.log(error)
+            }
         }
     }
 
