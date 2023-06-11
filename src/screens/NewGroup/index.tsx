433ac8caba65eacda78ba1fee70fe57cea6e3917
Arley Souto <macx15@hotmail.com>
2023-06-10 23:10:19 -0300

    utiliando useRoute para recuperar dados

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/native'
 
 import * as S from './styles'
@@ -8,10 +8,13 @@ import { Button } from '@components/Button';
 import { Input } from '@components/Input';
 
 export const NewGroup: React.FC = () => {
+    const [group, setGroup] = useState('')
+
+    
     const navigation = useNavigation();
 
     function handleNew() {
-        navigation.navigate('players', { group: 'Rocket' })
+        navigation.navigate('players', { group })
     }
 
     return (
@@ -26,6 +29,8 @@ export const NewGroup: React.FC = () => {
                 />
                 <Input
                     placeholder='Nome da turma'
+                    value={group}
+                    onChangeText={setGroup}
                 />
                 <Button
                     title='Criar'
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { FlatList } from 'react-native';
+import { useRoute } from '@react-navigation/native'
 
 import * as S from './styles';
 import { Header } from '@components/Header';
@@ -11,14 +12,24 @@ import { PlayerCard } from '@components/PlayerCard';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
 
+type RouteParams = {
+  group: string;
+}
+
 export const Players: React.FC = () => {
   const [team, setTeam] = useState('Time A')
   const [players, setPlayers] = useState([])
 
+  const route = useRoute()
+  const { group } = route.params as RouteParams
+
   return (
     <S.Container>
       <Header showBackButton />
-      <Highlight title='Nome da turma' subtitle='adicione a galera e separe os times' />
+      <Highlight
+        title={group}
+        subtitle='adicione a galera e separe os times'
+      />
       <S.Form>
         <Input
           placeholder='Nome da pessoa'
@@ -48,18 +59,18 @@ export const Players: React.FC = () => {
         data={players}
         keyExtractor={item => item}
         renderItem={({ item }) => (
-          <PlayerCard name={item} onRemove={() =>{}} />
+          <PlayerCard name={item} onRemove={() => { }} />
         )}
         ListEmptyComponent={() => <ListEmpty message='Não há pessoas nesse time.' />}
         showsVerticalScrollIndicator={false}
         contentContainerStyle={[
-          {paddingBottom:100},
-          players.length === 0 && {flex:1}
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 }
         ]}
       />
       <Button
-      title='Remover Turma'
-      type='SECUNDARY'
+        title='Remover Turma'
+        type='SECUNDARY'
       />
 
     </S.Container>
